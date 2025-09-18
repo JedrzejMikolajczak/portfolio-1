4215891b8e260ac56615e24da41f75e18611cdeb
JedrzejMikolajczak <jedrzej.mikolajczak@gmail.com>
2025-09-18 21:15:53 +0200

    a

---
+++ script.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const scrollProjectsBtn = document.getElementById("project-btn")
+    const targetProjects = document.querySelector(".projects")
+
+    const scrollKontaktBtn = document.getElementById("kontakt-btn")
+    const targetKontakt = document.querySelector(".contact")
+
+    const scrollAboutBtn = document.getElementById("about-btn")
+    const targetAbout = document.querySelector(".about")
+
+    if(scrollProjectsBtn && targetProjects){
+        scrollProjectsBtn.addEventListener('click', function(event){
+            event.preventDefault();
+            targetProjects.scrollIntoView({behavior: 'smooth'})
+        })
+    }
+    if(scrollKontaktBtn && targetKontakt){
+        scrollKontaktBtn.addEventListener('click', function(event){
+            event.preventDefault();
+            targetKontakt.scrollIntoView({behavior: 'smooth'})
+        })
+    }
+    if(scrollAboutBtn && targetAbout){
+        scrollAboutBtn.addEventListener('click', function(event){
+            event.preventDefault();
+            targetAbout.scrollIntoView({behavior: 'smooth'})
+        })
+    }
+      const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove('active', 'prev', 'next');
+    if (i === index) {
+      slide.classList.add('active');
+    } else if (i === (index - 1 + slides.length) % slides.length) {
+      slide.classList.add('prev');
+    } else if (i === (index + 1) % slides.length) {
+      slide.classList.add('next');
+    }
+  });
+}
+
+setInterval(() => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}, 6000);
+
+showSlide(currentSlide);
+  
+})
